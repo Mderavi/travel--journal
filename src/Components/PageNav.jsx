decd212fbb2ee05240cd9896335bf8bc06c54e04
Mderavi <deravi.mahsa@gmail.com>
2025-06-21 13:50:53 +1000

    connect the login page and add pageNav

--- src/Components/PageNav.jsx
+++ src/Components/PageNav.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
+
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
         <li>
           <Link to="/pricing">Pricing</Link>
         </li>
         <li>
           <Link to="/product">Product</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
         </li>
       </ul>
     </nav>
